76b4912559833c0fea0ead2752a8a6e6f70bf81e
Shaffy <thedave2cz@gmail.com>
2026-04-16 18:13:55 +0200

    got func_080001D4

---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+void func_080001D4(void) {
+    func_0800774C();
+    func_08002620();
+    func_08006148(D_083A3D94 + 0xBF1, 0x38000);
+    
+    func_0800567C();
+    func_08003DA0();
+    func_08003E64();
+    func_08006F28();
+    func_08007EA0();
+    func_080004DC();
+    if (func_080005DC() != 0) {
+        if (func_080005F0() != 0) {
+            func_080004F0();
+        } else {
+            func_08000634();
+        }
+    }
+    func_08000648();
+    func_0800A27C();
+    func_08005570();
+    func_080109B4();
+    func_08000F10();
+    D_03006514 = 1;
+}
---
+++ src/main.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "global.h"
+
+extern void func_0800774C();
+extern void func_08002620();
+extern void func_08006148(u32* arg0, u32 arg1);
+extern void func_0800567C();
+extern void func_08003DA0();
+extern void func_08003E64();
+extern void func_08006F28();
+extern void func_08007EA0();
+extern void func_080004DC();
+extern u32 func_080005DC();
+extern u32 func_080005F0();
+extern void func_080004F0();
+extern void func_08000634();
+extern void func_08000648();
+extern void func_0800A27C();
+extern void func_08005570();
+extern void func_080109B4();
+extern void func_08000F10();
+
+extern u32* D_083A3D94;
+extern u8 D_03006514;
